e3e8372506ec6d56e92bb89c6f4ed38084fb2d3b
bang-wol <milk011922@gmail.com>
2024-07-18 17:31:48 +0900

    feat: 라우터 설정 추가
    - 각 컴포넌트로의 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import Read from "./pages/Read";
+import Reading from "./pages/Reading";
+import Wish from "./pages/Wish";
 
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/read" element={<Read />} />
+        <Route path="/reading" element={<Reading />} />
+        <Route path="/wish" element={<Wish />} />
+      </Route>
+    </Routes>
   );
 }
 
