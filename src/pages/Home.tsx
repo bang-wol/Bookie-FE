8a901199bde13290bab5ab4e0aa536f85655c6f0
bang-wol <milk011922@gmail.com>
2024-07-18 18:38:29 +0900

    feat: Home 컴포넌트에서 책 검색 기능 구현
    - BookSearchForm과 BookList 추가
    - axios를 사용하여 API 호출 후 검색 결과 상태로 관리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import BookSearchForm from "../components/BookSearchForm";
+import axios from "../api/axiosConfig";
+import BookList from "../components/BookList";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home: React.FC = () => {
+  const [books, setBooks] = useState([]);
+
+  const handleSearch = async (query: string) => {
+    try {
+      const response = await axios.get("/books/search", {
+        params: { query },
+      });
+      setBooks(response.data);
+    } catch (error) {
+      console.error("Error fetching books", error);
+    }
+  };
+
+  return (
+    <Container>
+      <BookSearchForm onSearch={handleSearch} />
+      <BookList books={books} />
+    </Container>
+  );
 };
+
+const Container = styled.div``;
 
 export default Home;
